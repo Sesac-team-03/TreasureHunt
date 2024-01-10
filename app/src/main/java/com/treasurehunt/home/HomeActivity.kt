247050f2a0ce7a5f786c67a0e9a20d5306686847
jungkeunking <jungkeunking@gmail.com>
2024-01-10 17:15:15 +0900

    rename: MainActivity -> HomeActivity 이름 변경 및 home 패키지로 이동
    Co-Authored-By: 8946SJL <144574943+8946SJL@users.noreply.github.com>
    Co-Authored-By: 김주한 <83435602+juhan2103@users.noreply.github.com>
    Co-Authored-By: 윤동현 <82762769+donghyun81@users.noreply.github.com>

--- app/src/main/java/com/treasurehunt/home/HomeActivity.kt
+++ app/src/main/java/com/treasurehunt/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.treasurehunt
+package com.treasurehunt.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.treasurehunt.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
