3a9b0870c17c7bce36ae9fcbbb4d82a55e3c9ab5
jungkeunking <jungkeunking@gmail.com>
2024-01-10 19:50:21 +0900

    feat: 홈 액티비티에 NavHostFragment 및 navController 지정

--- app/src/main/java/com/treasurehunt/home/HomeActivity.kt
+++ app/src/main/java/com/treasurehunt/home/HomeActivity.kt
@@ -2,11 +2,21 @@ package com.treasurehunt.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.treasurehunt.R
+import com.treasurehunt.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_home) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
