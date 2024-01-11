50175dc72ed5da4eb91a60d44b04d4b12c42b02a
김주한 <83435602+juhan2103@users.noreply.github.com>
2024-01-11 17:57:56 +0900

    feat: 앨범에서 이미지 선택하고 imageView에 나타내는 기능 구현

--- app/src/main/java/com/treasurehunt/RecordFragment.kt
+++ app/src/main/java/com/treasurehunt/RecordFragment.kt
@@ -1,15 +1,56 @@
 package com.treasurehunt
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.treasurehunt.databinding.FragmentRecordBinding
+import java.io.File
+import java.net.URL
 
 class RecordFragment : Fragment() {
     private var _binding: FragmentRecordBinding? = null
     private val binding get() = _binding!!
+    private val recordAdapter = RecordAdapter()
+    private val imageLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val selectedImages = mutableListOf<Uri>()
+
+                if (result.data?.clipData != null) {
+                    val count = result.data?.clipData!!.itemCount
+                    if (count > 5) {
+                        Snackbar.make(
+                            binding.root,
+                            getString(R.string.record_sb_max_count),
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                    }
+                    for (i in 0 until count) {
+                        selectedImages.add(result.data?.clipData!!.getItemAt(i).uri)
+                    }
+                } else if (result.data?.data != null) {
+                    selectedImages.add(result.data!!.data!!)
+                }
+                handleSelectedImages(selectedImages)
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +63,25 @@ class RecordFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.rvPhoto.adapter = recordAdapter
+        binding.ibSelectPhoto.setOnClickListener {
+            openGallery()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        imageLauncher.launch(intent)
+    }
+
+    private fun handleSelectedImages(uris: List<Uri>?) {
+        recordAdapter.submitList(uris)
     }
 }
