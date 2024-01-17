cd4d8f5ff6d4d8058c14c97e3abd566346657889
김주한 <83435602+juhan2103@users.noreply.github.com>
2024-01-17 17:53:33 +0900

    feat: 지도 스냅샷 표시

--- app/src/main/java/com/treasurehunt/ui/savelog/SaveLogFragment.kt
+++ app/src/main/java/com/treasurehunt/ui/savelog/SaveLogFragment.kt
@@ -1,16 +1,24 @@
 package com.treasurehunt.ui.savelog
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
+import com.treasurehunt.R
 import com.treasurehunt.databinding.FragmentSavelogBinding
 import com.treasurehunt.ui.savelog.adapter.SaveLogAdapter
 
-class SaveLogFragment : Fragment() {
+class SaveLogFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentSavelogBinding? = null
     private val binding get() = _binding!!
@@ -28,6 +36,14 @@ class SaveLogFragment : Fragment() {
             }
         }
 
+    private lateinit var map: NaverMap
+    private val source: FusedLocationSource =
+        FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            setLocationTrackingMode(isGranted)
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -43,6 +59,7 @@ class SaveLogFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
         setAddImage()
+        loadMap()
     }
 
     override fun onDestroyView() {
@@ -58,5 +75,50 @@ class SaveLogFragment : Fragment() {
         binding.ibSelectPhoto.setOnClickListener {
             imageLauncher.launch(viewModel.getImage())
         }
+    }
+
+    private fun setLocationTrackingMode(isGranted: Boolean) {
+        map.locationTrackingMode =
+            if (isGranted) {
+                LocationTrackingMode.Follow
+            } else {
+                LocationTrackingMode.None
+            }
+    }
+
+    private fun initMap(naverMap: NaverMap) {
+        map = naverMap.apply {
+            locationSource = source
+            uiSettings.isZoomControlEnabled = false
+        }
+    }
+
+    private fun loadMap() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.fcv_map) as MapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun handleLocationAccessPermission() {
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) -> {
+                setLocationTrackingMode(true)
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            }
+        }
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        initMap(naverMap)
+        handleLocationAccessPermission()
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1001
     }
 }
