deb0141a3c237bf003568842d1c4146b82070011
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-01-17 18:22:06 +0900

    feat: 네이버 정보를 통한 firebase 회원가입 구현

--- app/src/main/java/com/treasurehunt/ui/login/LogInFragment.kt
+++ app/src/main/java/com/treasurehunt/ui/login/LogInFragment.kt
@@ -2,6 +2,7 @@ package com.treasurehunt.ui.login
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +12,11 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.navercorp.nid.NaverIdLoginSDK
 import com.navercorp.nid.oauth.NidOAuthBehavior
+import com.navercorp.nid.oauth.NidOAuthLogin
 import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileMap
+import com.navercorp.nid.profile.data.NidProfileResponse
 import com.treasurehunt.BuildConfig
 import com.treasurehunt.R
 import com.treasurehunt.databinding.FragmentLoginBinding
@@ -55,15 +60,12 @@ class LogInFragment : Fragment() {
         binding.btnNaverLogin.setOnClickListener {
             NaverIdLoginSDK.behavior = NidOAuthBehavior.NAVERAPP
             NaverIdLoginSDK.authenticate(requireContext(), object : OAuthLoginCallback {
-                override fun onSuccess() {}
+                override fun onSuccess() {
+                    createNaverAccount()
+                    findNavController().navigate(R.id.action_logInFragment_to_homeFragment)
+                }
+
                 override fun onFailure(httpStatus: Int, message: String) {
-                    val errorCode = NaverIdLoginSDK.getLastErrorCode().code
-                    val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
-                    Toast.makeText(
-                        context,
-                        "errorCode:$errorCode, errorDesc:$errorDescription",
-                        Toast.LENGTH_SHORT
-                    ).show()
                 }
 
                 override fun onError(errorCode: Int, message: String) {
@@ -74,17 +76,42 @@ class LogInFragment : Fragment() {
         }
     }
 
+    private fun createNaverAccount() {
+        NidOAuthLogin().callProfileApi(object : NidProfileCallback<NidProfileResponse> {
+            override fun onSuccess(response: NidProfileResponse) {
+                Toast.makeText(context, "$response", Toast.LENGTH_SHORT).show()
+                createAccount(response.profile?.email!!, response.profile?.id!!)
+            }
+
+            override fun onFailure(httpStatus: Int, message: String) {
+            }
+
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
+        })
+    }
+
+    private fun createAccount(email: String, password: String) {
+        viewModel.auth.observe(viewLifecycleOwner) {
+            it.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("12345", "createUserWithEmail:success")
+                    } else {
+                        Log.w("12345", "createUserWithEmail:failure", task.exception)
+                    }
+                }
+        }
+
+    }
+
     private fun guestLogin() {
         binding.btnGuestLogin.setOnClickListener {
             viewModel.auth.observe(viewLifecycleOwner) {
                 it.signInAnonymously()
                     .addOnCompleteListener(Activity()) { task ->
                         if (task.isSuccessful) {
-                            Toast.makeText(
-                                requireContext(),
-                                "Authentication Success.",
-                                Toast.LENGTH_SHORT,
-                            ).show()
                         } else {
                             // If sign in fails, display a message to the user.
                             Toast.makeText(
