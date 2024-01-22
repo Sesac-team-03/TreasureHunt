4d09903cd5f6afb41308398a91a224f34c45f808
8946SJL <eutume5@gmail.com>
2024-01-22 14:21:29 +0900

    feat: handler -> coroutines으로 딜레이 구현 방식 변경

--- app/src/main/java/com/treasurehunt/ui/login/SplashFragment.kt
+++ app/src/main/java/com/treasurehunt/ui/login/SplashFragment.kt
@@ -1,15 +1,16 @@
 package com.treasurehunt.ui.login
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.treasurehunt.R
 import com.treasurehunt.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
 
@@ -34,9 +35,10 @@ class SplashFragment : Fragment() {
         binding.animationView.setAnimation(R.raw.iv_treasure)
         binding.animationView.playAnimation()
 
-        Handler(Looper.getMainLooper()).postDelayed({
+        lifecycleScope.launch {
+            delay(2000)
             findNavController().navigate(R.id.action_splashFragment_to_logInFragment)
-        }, 4000)
+        }
     }
 
     override fun onDestroyView() {
