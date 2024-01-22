b45ef3fc77388777712568c7ee18cb1238696704
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-01-22 12:48:04 +0900

    feat: RoomDatabase 비지니스로직 추가
    -  제가 임시로하면 conflic이 많이 생길것 같아서 정근님이 작업하신 부분 가져왔습니다.

---
+++ app/src/main/java/com/treasurehunt/data/local/Converters.kt
@@ -0,0 +1,13 @@
+package com.treasurehunt.data.local
+
+import androidx.room.TypeConverter
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class Converters {
+    @TypeConverter
+    fun fromString(value: String): List<String> = Json.decodeFromString(value)
+
+    @TypeConverter
+    fun listToString(value: List<String>): String = Json.encodeToString(value)
+}
