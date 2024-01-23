15f8c00380ce96ca3af292bb5f44070b25150cbd
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-01-23 15:51:45 +0900

    build: ksp 최신 버전으로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,5 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 
-kapt {
-    correctErrorTypes = true
-}
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
