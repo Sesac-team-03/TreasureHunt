ff669c17c48ea0a0658a354920aa64cff0c05ee0
김주한 <83435602+juhan2103@users.noreply.github.com>
2024-01-25 23:24:21 +0900

    feat: 프로필 수정 기능 구현
    수정 후 바로 싱크되도록 구현 필요

--- app/src/main/java/com/treasurehunt/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/treasurehunt/ui/profile/ProfileFragment.kt
@@ -1,21 +1,39 @@
 package com.treasurehunt.ui.profile
 
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.bumptech.glide.Glide
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.storage.storage
+import com.treasurehunt.R
+import com.treasurehunt.data.remote.model.UserDTO
 import com.treasurehunt.databinding.FragmentProfileBinding
+import com.treasurehunt.util.showSnackbar
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
     private val viewModel: ProfileViewModel by viewModels { ProfileViewModel.Factory }
+    private val imageLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            setImageLauncher(result)
+        }
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            handleAccessAlbum(isGranted)
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,11 +41,14 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setAlbumPermission()
         setEditButton()
         initProfile()
     }
@@ -37,35 +58,105 @@ class ProfileFragment : Fragment() {
         _binding = null
     }
 
-    private fun setEditButton() {
-        binding.ibEdit.setOnClickListener {
-            binding.tvCancel.visibility = View.VISIBLE
-            binding.tvCompleted.visibility = View.VISIBLE
-            binding.ibEdit.visibility = View.GONE
-        }
-        binding.tvCancel.setOnClickListener {
-            binding.tvCancel.visibility = View.GONE
-            binding.tvCompleted.visibility = View.GONE
-            binding.ibEdit.visibility = View.VISIBLE
-        }
-        binding.tvCompleted.setOnClickListener {
-            binding.tvCancel.visibility = View.GONE
-            binding.tvCompleted.visibility = View.GONE
-            binding.ibEdit.visibility = View.VISIBLE
+    private fun setImageLauncher(result: ActivityResult) {
+        if (result.data?.data != null) {
+            val uri = result.data?.data
+            viewModel.addImage(uri.toString())
+            viewModel.setProfileUri(uri.toString())
         }
     }
 
-    private fun initProfile() {
-        lifecycleScope.launch {
-            if (viewModel.getUserData().email.isNotEmpty()) {
-                val profileImage = viewModel.getUserData().profileImage
-                val nickname = viewModel.getUserData().nickname
-                val email = viewModel.getUserData().email
-                Glide.with(requireContext()).load(profileImage).into(binding.ivProfileImage)
-                binding.tvNickname.text = nickname
-                binding.tvAccount.text = email
+    private fun handleAccessAlbum(isGranted: Boolean) {
+        if (isGranted) {
+            imageLauncher.launch(viewModel.getImage())
+        }
+    }
+
+    private fun setAlbumPermission() {
+        binding.ibCamera.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                requestPermissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
             } else {
-                Log.d("ProfileFragment", viewModel.getUserData().toString())
+                requestPermissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+    private fun setEditButton() {
+        binding.ibEdit.setOnClickListener {
+            showEditView()
+        }
+        binding.tvCancel.setOnClickListener {
+            hideEditView()
+        }
+        binding.tvCompleted.setOnClickListener {
+            if (binding.etNickname.text.isEmpty()) {
+                return@setOnClickListener
+            } else {
+                saveProfile()
+                hideEditView()
+            }
+        }
+    }
+
+    private fun showEditView() {
+        binding.ibEdit.visibility = View.GONE
+        binding.tvNickname.visibility = View.GONE
+        binding.tvCancel.visibility = View.VISIBLE
+        binding.tvCompleted.visibility = View.VISIBLE
+        binding.ibCamera.visibility = View.VISIBLE
+        binding.etNickname.visibility = View.VISIBLE
+    }
+
+    private fun hideEditView() {
+        binding.ibEdit.visibility = View.VISIBLE
+        binding.tvNickname.visibility = View.VISIBLE
+        binding.tvCancel.visibility = View.GONE
+        binding.tvCompleted.visibility = View.GONE
+        binding.ibCamera.visibility = View.GONE
+        binding.etNickname.visibility = View.GONE
+    }
+
+    private fun initProfile() {
+        viewModel.getUserData()
+    }
+
+    private suspend fun uploadProfileImage(uri: Uri) {
+        val uid = Firebase.auth.currentUser!!.uid
+        val storage = Firebase.storage
+        val storageRef = storage.getReference("${uid}/profile_image")
+        val fileName = uri.toString().replace("[^0-9]".toRegex(), "")
+        val mountainsRef = storageRef.child("${fileName}.png")
+        val uploadTask = mountainsRef.putFile(uri)
+        uploadTask.await()
+        uploadTask.addOnProgressListener { taskSnapshot ->
+            viewModel.setProfileUri(taskSnapshot.storage.toString())
+        }.addOnSuccessListener {
+            binding.root.showSnackbar(R.string.savelog_sb_upload_success)
+        }.addOnFailureListener {
+            binding.root.showSnackbar(R.string.savelog_sb_upload_failure)
+        }
+    }
+
+    private fun saveProfile() {
+        lifecycleScope.launch {
+            if (viewModel.imageUri.value.isNotEmpty()) {
+                uploadProfileImage(viewModel.imageUri.value.toUri())
+                viewModel.insertUserData(
+                    UserDTO(
+                        email = binding.tvAccount.text.toString(),
+                        nickname = binding.etNickname.text.toString(),
+                        profileImage = viewModel.profileUri.value
+                    )
+                )
+            } else {
+                viewModel.insertUserData(
+                    UserDTO(
+                        email = binding.tvAccount.text.toString(),
+                        nickname = binding.etNickname.text.toString(),
+                        profileImage = viewModel.profileUri.value
+                    )
+                )
             }
         }
     }
--- app/src/main/java/com/treasurehunt/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/treasurehunt/ui/profile/ProfileViewModel.kt
@@ -1,21 +1,75 @@
 package com.treasurehunt.ui.profile
 
+import android.content.Intent
+import android.service.autofill.UserData
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.treasurehunt.TreasureHuntApplication
+import com.treasurehunt.data.User
 import com.treasurehunt.data.UserRepository
 import com.treasurehunt.data.remote.model.UserDTO
+import com.treasurehunt.ui.savelog.ImageModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class ProfileViewModel(
     private val userRepository: UserRepository
 ) : ViewModel() {
 
-    suspend fun getUserData(): UserDTO {
+    private val _nicknameData: MutableStateFlow<String> = MutableStateFlow("")
+    val nicknameData = _nicknameData.asStateFlow()
+    private val _emailData: MutableStateFlow<String> = MutableStateFlow("")
+    val emailData = _emailData.asStateFlow()
+    private val _imageUri: MutableStateFlow<String> = MutableStateFlow("")
+    val imageUri = _imageUri.asStateFlow()
+    private val _profileUri: MutableStateFlow<String> = MutableStateFlow("")
+    val profileUri = _profileUri.asStateFlow()
+
+    private fun setNickname(nickname: String) {
+        _nicknameData.value = nickname
+    }
+
+    private fun setEmail(email: String) {
+        _emailData.value = email
+    }
+
+    fun setProfileUri(profileUri: String) {
+        _profileUri.value = profileUri
+    }
+
+    fun getImage(): Intent {
+        return Intent(Intent.ACTION_PICK).apply {
+            type = "image/"
+        }
+    }
+
+    fun addImage(image: String) {
+        _imageUri.value = image
+    }
+
+    fun getUserData() {
         val uid = Firebase.auth.currentUser!!.uid
-        return userRepository.getRemoteUser(uid)
+        viewModelScope.launch {
+            val user = userRepository.getRemoteUser(uid)
+            if (user.email.isEmpty()) {
+                setNickname(uid)
+            } else {
+                setNickname(user.nickname.toString())
+                setEmail(user.email)
+                setProfileUri(user.profileImage.toString())
+            }
+        }
+    }
+
+    suspend fun insertUserData(userDTO: UserDTO) {
+        userRepository.insert(Firebase.auth.currentUser!!.uid, userDTO)
     }
 
     companion object {
