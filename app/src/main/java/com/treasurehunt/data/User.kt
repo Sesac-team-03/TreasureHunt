394aef5aa1edf2d89cc84b5dbc17baf4695256de
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-01-31 13:59:04 +0900

    refactor: user의 email의 non null 수정

--- app/src/main/java/com/treasurehunt/data/User.kt
+++ app/src/main/java/com/treasurehunt/data/User.kt
@@ -1,7 +1,7 @@
 package com.treasurehunt.data
 
 data class User(
-    val email: String,
+    val email: String? = null,
     val id: String,
     val nickname: String? = null,
     val profileImage: String? = null
