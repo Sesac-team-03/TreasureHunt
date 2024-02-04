affe774d102c0b809ec63f0a611edd12d85d4d94
8946SJL <eutume5@gmail.com>
2024-02-04 18:21:12 +0900

    feat: 네트워크 연결위해 LogDTO를 LogModel로 변환

--- app/src/main/java/com/treasurehunt/data/remote/model/LogDTO.kt
+++ app/src/main/java/com/treasurehunt/data/remote/model/LogDTO.kt
@@ -1,6 +1,7 @@
 package com.treasurehunt.data.remote.model
 
 import com.treasurehunt.data.local.model.LogEntity
+import com.treasurehunt.ui.model.LogModel
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -15,4 +16,14 @@ data class LogDTO(
 fun LogDTO.toLogEntity(remoteId: String): LogEntity {
     val (place, images, text, theme, createdDate) = this
     return LogEntity(place, images.keys.toList(), text, theme, createdDate, remoteId)
+}
+
+fun LogDTO.asLogModel(): LogModel {
+    return LogModel(
+        place = this.place,
+        images = this.images.keys.toList(),
+        text = this.text,
+        theme = this.theme,
+        createdDate = this.createdDate
+    )
 }
