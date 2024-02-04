390b7a948c3dae05187fa2afab2ffccd5ad0737c
8946SJL <eutume5@gmail.com>
2024-02-04 18:21:48 +0900

    feat: LogModel에 Parcelable 적용

--- app/src/main/java/com/treasurehunt/ui/model/LogModel.kt
+++ app/src/main/java/com/treasurehunt/ui/model/LogModel.kt
@@ -1,5 +1,7 @@
 package com.treasurehunt.ui.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.treasurehunt.data.local.model.LogEntity
 import com.treasurehunt.data.remote.model.LogDTO
 
@@ -9,14 +11,36 @@ data class LogModel(
     val text: String,
     val theme: String,
     val createdDate: Long
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.createStringArrayList() ?: emptyList(),
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readLong()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(place)
+        parcel.writeStringList(images)
+        parcel.writeString(text)
+        parcel.writeString(theme)
+        parcel.writeLong(createdDate)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<LogModel> {
+        override fun createFromParcel(parcel: Parcel): LogModel = LogModel(parcel)
+
+        override fun newArray(size: Int): Array<LogModel?> = arrayOfNulls(size)
+    }
+}
 
 fun LogModel.asLogEntity(remoteId: String? = null, localId: Long = 0): LogEntity {
-    val (place, images, text, theme, createdDate) = this
-    return LogEntity(place, images, text, theme, createdDate)
+    return LogEntity(place, images, text, theme, createdDate, remoteId, id = localId)
 }
 
 fun LogModel.asLogDTO(remoteId: String? = null, localId: Long = 0): LogDTO {
-    val (place, images, text, theme, createdDate) = this
     return LogDTO(place, images.associateWith { true }, text, theme, createdDate)
 }
