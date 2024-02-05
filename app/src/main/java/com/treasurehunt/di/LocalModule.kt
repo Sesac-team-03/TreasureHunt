e937c3a110ff045157109d67b0c556c8c2af3911
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-02-05 18:12:40 +0900

    feat: 의존성 주입 모듈 생성

---
+++ app/src/main/java/com/treasurehunt/di/LocalModule.kt
@@ -0,0 +1,37 @@
+package com.treasurehunt.di
+
+import android.content.Context
+import androidx.room.Room
+import com.treasurehunt.data.local.TreasureHuntDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalModule {
+
+    @Singleton
+    @Provides
+    fun provideDataBase(@ApplicationContext context: Context): TreasureHuntDatabase {
+        return Room.databaseBuilder(
+            context,
+            TreasureHuntDatabase::class.java,
+            "db"
+        ).build()
+    }
+
+    @Provides
+    fun provideUserDao(database: TreasureHuntDatabase) = database.userDao()
+
+    @Provides
+    fun provideLogDao(database: TreasureHuntDatabase) = database.logDao()
+
+    @Provides
+    fun providePlaceDao(database: TreasureHuntDatabase) = database.placeDao()
+}
+
+
---
+++ app/src/main/java/com/treasurehunt/di/RepositoryModule.kt
@@ -0,0 +1,27 @@
+package com.treasurehunt.di
+
+import com.treasurehunt.data.LogRepository
+import com.treasurehunt.data.LogRepositoryImpl
+import com.treasurehunt.data.PlaceRepository
+import com.treasurehunt.data.PlaceRepositoryImpl
+import com.treasurehunt.data.UserRepository
+import com.treasurehunt.data.UserRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun bindUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository
+
+    @Binds
+    abstract fun bindLogRepository(logRepositoryImpl: LogRepositoryImpl): LogRepository
+
+    @Binds
+    abstract fun bindPlaceRepository(placeRepositoryImpl: PlaceRepositoryImpl): PlaceRepository
+
+}
