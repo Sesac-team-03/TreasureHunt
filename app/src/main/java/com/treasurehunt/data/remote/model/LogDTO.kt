bb89d8c842dca3b5072c36f94e1e31723e18019d
8946SJL <eutume5@gmail.com>
2024-02-11 14:56:04 +0900

    feat: logid 조회 및 getImage로 이미지 조회 함수 추가

--- app/src/main/java/com/treasurehunt/data/remote/model/LogDTO.kt
+++ app/src/main/java/com/treasurehunt/data/remote/model/LogDTO.kt
@@ -1,6 +1,8 @@
 package com.treasurehunt.data.remote.model
 
+import com.treasurehunt.data.ImageRepository
 import com.treasurehunt.data.local.model.LogEntity
+import com.treasurehunt.ui.model.LogModel
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -16,4 +18,9 @@ data class LogDTO(
 fun LogDTO.toLogEntity(remoteId: String): LogEntity {
     val (place, images, text, theme, createdDate, localId) = this
     return LogEntity(place, images.keys.toList(), text, theme, createdDate, remoteId, localId)
+}
+
+suspend fun LogDTO.toLogModel(imageRepo: ImageRepository): LogModel {
+    val imageUrls = images.keys.map { imageId -> imageRepo.getImage(imageId).url }
+    return LogModel(place, images.keys.toList(), imageUrls, text, theme, createdDate)
 }
