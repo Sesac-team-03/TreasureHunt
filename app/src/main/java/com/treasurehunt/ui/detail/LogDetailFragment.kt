cdb6aed931ab7ef7b6f3cf17dbb99e0aae2cff2e
8946SJL <eutume5@gmail.com>
2024-02-11 16:24:14 +0900

    feat: [피드백 반영] 함수명 첫글자 소문자로 변경

--- app/src/main/java/com/treasurehunt/ui/detail/LogDetailFragment.kt
+++ app/src/main/java/com/treasurehunt/ui/detail/LogDetailFragment.kt
@@ -120,7 +120,7 @@ class LogDetailFragment : BottomSheetDialogFragment() {
         val logModel = args.log
         if (placeId.isNotEmpty()) {
             viewLifecycleOwner.lifecycleScope.launch {
-                val logModel = viewModel.GetPlace(placeId)
+                val logModel = viewModel.getPlace(placeId)
                 logModel?.let {
                     updateUIWithLogModel(it)
                 }
