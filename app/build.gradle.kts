982d0906ee501e9ba02dc47f1af9ced33ea204b7
jungkeunking <jungkeunking@gmail.com>
2024-02-13 06:56:41 +0900

    build: 힐트 워크매니저 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,6 +111,9 @@ dependencies {
     ksp("com.google.dagger:hilt-android-compiler:2.48")
 
     implementation("androidx.work:work-runtime-ktx:2.9.0")
+
+    implementation("androidx.hilt:hilt-work:1.2.0-beta01")
+    ksp("androidx.hilt:hilt-compiler:1.2.0-beta01")
 }
 
 kapt {
