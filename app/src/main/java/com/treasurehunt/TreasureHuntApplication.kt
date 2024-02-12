360cef71d7c2e7cb5eddb733b8e32c3d3999fd6d
jungkeunking <jungkeunking@gmail.com>
2024-02-13 07:04:23 +0900

    feat: CustomWorkerFactory 클래스 추가 및 TreasureHuntApplication 클래스에 WorkManager Provider 구현

--- app/src/main/java/com/treasurehunt/TreasureHuntApplication.kt
+++ app/src/main/java/com/treasurehunt/TreasureHuntApplication.kt
@@ -1,7 +1,18 @@
 package com.treasurehunt
 
 import android.app.Application
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class TreasureHuntApplication : Application()
+class TreasureHuntApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: CustomWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
