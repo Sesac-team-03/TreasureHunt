cbb9d273d4eb6557226670a30f6106d2da1d2807
8946SJL <eutume5@gmail.com>
2024-02-14 15:09:00 +0900

    feat: 원격 userId SDK로 삭제테스트

--- app/src/main/java/com/treasurehunt/data/UserRepository.kt
+++ app/src/main/java/com/treasurehunt/data/UserRepository.kt
@@ -21,4 +21,6 @@ interface UserRepository {
     suspend fun delete(vararg users: UserEntity): Int
 
     suspend fun search(startAt: String, limit: Int = 10): Map<String, UserDTO>
+
+//    suspend fun deleteUser(userId: String)
 }
