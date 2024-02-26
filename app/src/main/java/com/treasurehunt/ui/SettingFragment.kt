2ef6f9d6afdab08e565ea75fb4dd11d2b3a547f4
김주한 <83435602+juhan2103@users.noreply.github.com>
2024-02-26 11:27:43 +0900

    feat: SettingFragment 생성 및 뒤로 가기 버튼 구현

---
+++ app/src/main/java/com/treasurehunt/ui/SettingFragment.kt
@@ -0,0 +1,41 @@
+package com.treasurehunt.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.treasurehunt.databinding.FragmentSettingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SettingFragment : Fragment() {
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        backProfile()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun backProfile() {
+        binding.ibBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+}
