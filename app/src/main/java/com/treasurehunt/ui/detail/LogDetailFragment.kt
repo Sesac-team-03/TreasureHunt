811710ba8c463d68e8136dc093186ad36c9cd884
8946SJL <eutume5@gmail.com>
2024-02-26 09:53:51 +0900

    popup 구현 및 삭제코드 적용

--- app/src/main/java/com/treasurehunt/ui/detail/LogDetailFragment.kt
+++ app/src/main/java/com/treasurehunt/ui/detail/LogDetailFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -27,7 +28,6 @@ const val DOMAIN_URI_PREFIX = "https://treasurehuntsesac.page.link"
 
 @AndroidEntryPoint
 class LogDetailFragment : BottomSheetDialogFragment() {
-
     private var _binding: FragmentLogdetailBinding? = null
     private val binding get() = _binding!!
     private lateinit var imageSliderAdapter: ImageSliderAdapter
@@ -35,7 +35,9 @@ class LogDetailFragment : BottomSheetDialogFragment() {
     private val args: LogDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLogdetailBinding.inflate(inflater, container, false)
         return binding.root
@@ -43,16 +45,11 @@ class LogDetailFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         setDotsIndicator()
         loadLog()
-        //setEditButton()
-        setDeleteButton()
+        setPopupMenu(binding.btnEdit)
         setCloseButton()
         setShareButton()
-//        binding.btnEdit.setOnClickListener {
-//            //setPopupMenu(it)
-//        }
     }
 
     override fun onDestroyView() {
@@ -72,12 +69,13 @@ class LogDetailFragment : BottomSheetDialogFragment() {
             viewLifecycleOwner.lifecycleScope.launch {
                 val log = viewModel.getLogByRemotePlaceId(placeId) ?: return@launch
                 setTextAndImages(log)
+                updateDotsIndicator()
             }
-        }
-
-        val log = args.log
-        if (log != null) {
-            setTextAndImages(log)
+        } else {
+            args.log?.let { log ->
+                setTextAndImages(log)
+                updateDotsIndicator()
+            }
         }
     }
 
@@ -87,29 +85,9 @@ class LogDetailFragment : BottomSheetDialogFragment() {
         imageSliderAdapter.submitList(imageItems)
     }
 
-    private fun setDeleteButton() {
-        binding.btnDelete.setOnClickListener {
-            val placeId = args.remotePlaceId
-            val userId = Firebase.auth.currentUser?.uid
-
-            viewLifecycleOwner.lifecycleScope.launch {
-                if (placeId.isNotEmpty() && userId != null) {
-                    val placeDTO = viewModel.getRemotePlace(placeId)
-                    placeDTO.log?.let { logId ->
-                        viewModel.deleteLogAndAssociatedData(logId, placeId, userId)
-                        Toast.makeText(requireContext(), "삭제", Toast.LENGTH_SHORT).show()
-                        dismiss()
-                    } ?: run {
-                        Toast.makeText(requireContext(), "삭제 실패", Toast.LENGTH_SHORT).show()
-                    }
-                } else {
-                    Toast.makeText(requireContext(), "삭제 처리 실패", Toast.LENGTH_SHORT).show()
-                }
-                val action =
-                    LogDetailFragmentDirections.actionLogDetailFragmentToHomeFragment(args.remotePlaceId)
-                findNavController().navigate(action)
-            }
-        }
+    private fun updateDotsIndicator() {
+        binding.vpPhoto.adapter = imageSliderAdapter
+        binding.diText.setViewPager2(binding.vpPhoto)
     }
 
     private fun setCloseButton() {
@@ -139,46 +117,53 @@ class LogDetailFragment : BottomSheetDialogFragment() {
             .setLink(Uri.parse(LINK_URL))
             .setDomainUriPrefix(DOMAIN_URI_PREFIX)
             .setAndroidParameters(
-                DynamicLink.AndroidParameters.Builder()
-                    .setMinimumVersion(1)
-                    .build()
+                DynamicLink.AndroidParameters.Builder().setMinimumVersion(1).build()
             )
             .buildDynamicLink()
 
         return dynamicLink.uri.toString()
     }
 
-//    private fun setPopupMenu(view: View) {
-//        val popup = PopupMenu(requireContext(), view)
-//        popup.menuInflater.inflate(R.menu.edit_menu, popup.menu)
-//
-//        popup.setOnMenuItemClickListener { menuItem ->
-//            when (menuItem.itemId) {
-//                R.id.action_edit -> {
-//                    true
-//                }
-//                R.id.action_delete -> {
-//                    val placeId = args.placeId
-//                    val userId = Firebase.auth.currentUser?.uid
-//
-//                    if (placeId.isNotEmpty() && userId != null) {
-//                        viewModel.viewModelScope.launch {
-//                            val placeDTO = viewModel.getRemotePlace(placeId)
-//                            placeDTO.log?.let { logId ->
-//                                viewModel.deletePost(placeId, userId, logId)
-//                                Toast.makeText(requireContext(), "삭제", Toast.LENGTH_SHORT).show()
-//                                dismiss()
-//                            } ?: run {
-//                                Toast.makeText(requireContext(), "삭제 실패", Toast.LENGTH_SHORT).show()
-//                            }
-//                        }
-//                    } else {
-//                        Toast.makeText(requireContext(), "삭제 처리 실패", Toast.LENGTH_SHORT).show()
-//                    }
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
-//    }
+    private fun setPopupMenu(view: View) {
+        val popup = PopupMenu(requireContext(), view)
+        popup.menuInflater.inflate(R.menu.edit_menu, popup.menu)
+
+        popup.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_edit -> {
+                    binding.btnEdit.setOnClickListener {
+                        findNavController().navigate(R.id.action_logDetailFragment_to_saveLogFragment)
+                    }
+                    true
+                }
+
+                R.id.action_delete -> {
+                    val placeId = args.remotePlaceId
+                    val userId = Firebase.auth.currentUser?.uid
+
+                    if (placeId.isNotEmpty() && userId != null) {
+                        viewLifecycleOwner.lifecycleScope.launch {
+                            val placeDTO = viewModel.getRemotePlace(placeId)
+                            placeDTO.log?.let { logId ->
+                                viewModel.deleteLogAndAssociatedData(logId, placeId, userId)
+                                Toast.makeText(requireContext(), "삭제", Toast.LENGTH_SHORT).show()
+                                dismiss()
+                            } ?: run {
+                                Toast.makeText(requireContext(), "삭제 실패", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    } else {
+                        Toast.makeText(requireContext(), "삭제 처리 실패", Toast.LENGTH_SHORT).show()
+                    }
+                    true
+                }
+
+                else -> false
+            }
+            false
+        }
+
+        view.setOnClickListener { popup.show() }
+    }
+
 }
