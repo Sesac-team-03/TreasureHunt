c28ad6db3cb363fab59d41fa93d465c49414f848
김주한 <83435602+juhan2103@users.noreply.github.com>
2024-03-07 21:53:31 +0900

    fix: 설정화면으로 갈 때 강제 종료되는 버그 수정
    DataStore가 여러곳에서 사용되는 문제가 발생했는데 DataStore에 의존성 주입을 하여 문제를 해결

--- app/src/main/java/com/treasurehunt/di/LocalModule.kt
+++ app/src/main/java/com/treasurehunt/di/LocalModule.kt
@@ -1,6 +1,9 @@
 package com.treasurehunt.di
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.room.Room
 import com.treasurehunt.data.local.TreasureHuntDatabase
 import dagger.Module
@@ -9,6 +12,9 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+
+private const val AUTO_LOGIN = "auto_login"
+private val Context.loginDataStore: DataStore<Preferences> by preferencesDataStore(name = AUTO_LOGIN)
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -35,4 +41,10 @@ object LocalModule {
 
     @Provides
     fun provideImageDao(database: TreasureHuntDatabase) = database.imageDao()
+
+    @Singleton
+    @Provides
+    fun provideLoginDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return context.loginDataStore
+    }
 }
