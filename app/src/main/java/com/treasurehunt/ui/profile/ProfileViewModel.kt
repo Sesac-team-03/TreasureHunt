d59f9abc9b459cef834ab88fd61c823efe17e478
jungkeunking <jungkeunking@gmail.com>
2024-03-10 19:45:35 +0900

    feat: 프로필 뷰모델에서 uid 및 user를 콜백플로우로 구현해 관리

--- app/src/main/java/com/treasurehunt/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/treasurehunt/ui/profile/ProfileViewModel.kt
@@ -6,24 +6,43 @@ import android.provider.MediaStore
 import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
-import com.google.firebase.storage.storage
+import com.google.firebase.FirebaseException
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.treasurehunt.BuildConfig
 import com.treasurehunt.data.UserRepository
+import com.treasurehunt.data.remote.REMOTE_DATABASE_USERS
 import com.treasurehunt.data.remote.model.UserDTO
 import com.treasurehunt.ui.model.ProfileUiState
+import com.treasurehunt.util.COMPILATION_WARNING_UNCHECKED_CAST
 import com.treasurehunt.util.FILENAME_EXTENSION_PNG
 import com.treasurehunt.util.MIME_TYPE_IMAGE
 import com.treasurehunt.util.STORAGE_LOCATION_PROFILE_IMAGE
 import com.treasurehunt.util.extractDigits
+import com.treasurehunt.util.getUidCallbackFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
+
+private const val BASE_URL = BuildConfig.BASE_URL
+private val pathEmail = UserDTO::email.name
+private val pathNickname = UserDTO::nickname.name
+private val pathProfileImage = UserDTO::profileImage.name
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
@@ -32,33 +51,73 @@ class ProfileViewModel @Inject constructor(
 
     private val _uiState: MutableStateFlow<ProfileUiState> = MutableStateFlow(ProfileUiState())
     val uiState: StateFlow<ProfileUiState> = _uiState.asStateFlow()
+    private val uid: Flow<String?> = getUidCallbackFlow()
+    private val user: Flow<UserDTO?> = uid.convertToUserFlow()
 
     init {
-        viewModelScope.launch {
-            initUid()
-            syncUid()
-            syncUser()
+        initUid()
+        initUser()
+    }
+
+    private fun Flow<String?>.convertToUserFlow() = transform { uid ->
+        if (uid == null) {
+            emit(null)
+        } else {
+            emitAll(getRemoteUserFlow(uid))
         }
+    }
+
+    private fun getRemoteUserFlow(uid: String): Flow<UserDTO?> = callbackFlow {
+        val db = FirebaseDatabase.getInstance(BASE_URL)
+        val userRef = db.reference.child(REMOTE_DATABASE_USERS).child(uid)
+        val callback = object : ValueEventListener {
+            @Suppress(COMPILATION_WARNING_UNCHECKED_CAST)
+            override fun onDataChange(snapshot: DataSnapshot) {
+                snapshot.run {
+                    val email = child(pathEmail).value as? String?
+                    val nickname = child(pathNickname).value as? String?
+                    val profileImage = child(pathProfileImage).value as? String?
+                    trySendBlocking(
+                        UserDTO(
+                            email = email,
+                            nickname = nickname,
+                            profileImage = profileImage,
+                        )
+                    )
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        }
+
+        try {
+            userRef.addValueEventListener(callback)
+        } catch (e: FirebaseException) {
+            trySendBlocking(null)
+            channel.close()
+        }
+
+        awaitClose { userRef.removeEventListener(callback) }
     }
 
     private fun initUid() {
-        _uiState.update {
-            it.copy(uid = Firebase.auth.currentUser?.uid)
-        }
-    }
-
-    private fun syncUid() {
-        Firebase.auth.addAuthStateListener { auth ->
-            _uiState.update {
-                it.copy(uid = auth.currentUser?.uid)
+        viewModelScope.launch {
+            uid.collect { uid ->
+                _uiState.update {
+                    it.copy(uid = uid)
+                }
             }
         }
     }
 
-    private suspend fun syncUser() {
-        val uid = _uiState.value.uid ?: return
-        _uiState.update {
-            it.copy(user = userRepo.getRemoteUserById(uid))
+    private fun initUser() {
+        viewModelScope.launch {
+            user.collect { user ->
+                _uiState.update {
+                    it.copy(user = user)
+                }
+            }
         }
     }
 
@@ -87,8 +146,6 @@ class ProfileViewModel @Inject constructor(
                 profileImage = _uiState.value.imageStorageUrl
             )
         )
-
-        syncUser() // not needed if syncUser operates on callbackflow
     }
 
     private suspend fun uploadProfileImage(uri: Uri) {
@@ -128,4 +185,3 @@ class ProfileViewModel @Inject constructor(
         }
     }
 }
-
--- app/src/main/java/com/treasurehunt/util/Functions.kt
+++ app/src/main/java/com/treasurehunt/util/Functions.kt
@@ -1,12 +1,37 @@
 package com.treasurehunt.util
 
 import android.os.Build
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.util.Date
 
-fun getCurrentTime() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+fun getCurrentTime(): Long = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
     LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
 } else {
     Date().time
+}
+
+internal fun getUidCallbackFlow(): Flow<String?> = callbackFlow {
+    val callback = FirebaseAuth.AuthStateListener { auth ->
+        trySendBlocking(auth.currentUser?.uid)
+    }
+
+    try {
+        Firebase.auth.addAuthStateListener(callback)
+    } catch (e: FirebaseException) {
+        trySendBlocking(null)
+        channel.close()
+    }
+
+    awaitClose {
+        Firebase.auth.removeAuthStateListener(callback)
+    }
 }
