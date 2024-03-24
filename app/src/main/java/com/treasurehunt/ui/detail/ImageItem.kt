42c2ec193a6e17185860c7d7015e486e65c8f9f6
jungkeunking <jungkeunking@gmail.com>
2024-03-24 20:39:28 +0900

    refactor: 기록 상세 화면의 어답터에 ViewType 적용 및 클래스명, 변수명 등 수정

--- app/src/main/java/com/treasurehunt/ui/detail/ImageItem.kt
+++
@@ -1,9 +0,0 @@
-package com.treasurehunt.ui.detail
-
-import com.treasurehunt.ui.model.TextTheme
-
-sealed class ImageItem {
-    data class Url(val url: String) : ImageItem()
-
-    data class Text(val value: String, val theme: TextTheme) : ImageItem()
-}
--- app/src/main/java/com/treasurehunt/ui/detail/LogDetailFragment.kt
+++ app/src/main/java/com/treasurehunt/ui/detail/LogDetailFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
 import android.widget.Toast
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -18,9 +17,10 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.dynamiclinks.DynamicLink
 import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.treasurehunt.R
 import com.treasurehunt.databinding.FragmentLogdetailBinding
-import com.treasurehunt.ui.detail.adapter.ImageSliderAdapter
+import com.treasurehunt.ui.detail.adapter.LogDetailViewPagerAdapter
 import com.treasurehunt.ui.model.LogModel
 import com.treasurehunt.ui.model.TextTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,9 +32,10 @@ const val MIME_TYPE_TEXT_PLAIN = "text/plain"
 
 @AndroidEntryPoint
 class LogDetailFragment : BottomSheetDialogFragment() {
+
     private var _binding: FragmentLogdetailBinding? = null
     private val binding get() = _binding!!
-    private lateinit var imageSliderAdapter: ImageSliderAdapter
+    private val logDetailViewPagerAdapter = LogDetailViewPagerAdapter()
     private val viewModel: LogDetailViewModel by viewModels()
 
     override fun onCreateView(
@@ -46,7 +47,8 @@ class LogDetailFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setImageSliderAdapter()
+        setAdapter()
+        setDotsIndicator()
         loadLog()
         setPopupButton(getPopupMenu())
         setCloseButton()
@@ -58,8 +60,12 @@ class LogDetailFragment : BottomSheetDialogFragment() {
         _binding = null
     }
 
-    private fun setImageSliderAdapter() {
-        imageSliderAdapter = ImageSliderAdapter()
+    private fun setAdapter() {
+        binding.vpImage.adapter = logDetailViewPagerAdapter
+    }
+
+    private fun setDotsIndicator() {
+        binding.diImage.setViewPager2(binding.vpImage)
     }
 
     private fun loadLog() {
@@ -68,7 +74,11 @@ class LogDetailFragment : BottomSheetDialogFragment() {
                 when (logResult) {
                     is LogResult.LogLoaded -> {
                         hideLoadingBar()
-                        setTextAndImages(logResult.value)
+                        if (logResult.value.imageUrls.isEmpty()) {
+                            setThemedText(logResult.value)
+                        } else {
+                            setTextAndImages(logResult.value)
+                        }
                         setDotsIndicator()
                     }
 
@@ -98,38 +108,37 @@ class LogDetailFragment : BottomSheetDialogFragment() {
     }
 
     private fun setTextAndImages(log: LogModel) {
-        if (log.imageUrls.isEmpty()) {
-            val imageItem = ImageItem.Text(value = log.text, theme = TextTheme.entries[log.theme])
-            imageSliderAdapter.submitList(listOf(imageItem))
-            return
-        }
+        val imageItem = LogDetailItem.TextItem(
+            value = log.text,
+            theme = TextTheme.entries[log.theme]
+        )
+        logDetailViewPagerAdapter.submitList(listOf(imageItem))
+    }
 
-        setTextTheme(TextTheme.entries[log.theme])
+    private fun setThemedText(log: LogModel) {
+        val item = LogDetailItem.TextItem(
+            value = log.text,
+            theme = TextTheme.entries[log.theme]
+        )
+        logDetailViewPagerAdapter.submitList(listOf(item))
+    }
+
+    private fun setImagesAndText(log: LogModel) {
+        val items = log.imageUrls.map {
+            LogDetailItem.ImageItem(storageReference = Firebase.storage.getReferenceFromUrl(it))
+        }
+        logDetailViewPagerAdapter.submitList(items)
         binding.tvText.text = log.text
-        val imageItems = log.imageUrls.map { ImageItem.Url(it) }
-        imageSliderAdapter.submitList(imageItems)
-    }
-
-    private fun setDotsIndicator() {
-        binding.vpPhoto.adapter = imageSliderAdapter
-        binding.diText.setViewPager2(binding.vpPhoto)
-    }
-
-    private fun setTextTheme(theme: TextTheme) {
-        binding.tvText.background = theme.backgroundResId?.let {
-            AppCompatResources.getDrawable(requireContext(), it)
-        }
-        binding.tvText.setTextColor(requireContext().getColor(theme.textColorResId))
     }
 
     private fun setPopupButton(popup: PopupMenu) {
-        binding.btnPopup.setOnClickListener {
+        binding.ibPopup.setOnClickListener {
             popup.show()
         }
     }
 
     private fun getPopupMenu(): PopupMenu {
-        return PopupMenu(requireContext(), binding.btnPopup).apply {
+        return PopupMenu(requireContext(), binding.ibPopup).apply {
             menuInflater.inflate(R.menu.edit_menu, menu)
 
             setOnMenuItemClickListener { menuItem ->
@@ -161,9 +170,10 @@ class LogDetailFragment : BottomSheetDialogFragment() {
                 val mapSymbol = viewModel.run {
                     getMapSymbol(args.remotePlaceId, args.log)
                 }
-                val action = LogDetailFragmentDirections.actionLogDetailFragmentToSaveLogFragment(
-                    mapSymbol, logResult.value
-                )
+                val action =
+                    LogDetailFragmentDirections.actionLogDetailFragmentToSaveLogFragment(
+                        mapSymbol, logResult.value
+                    )
                 findNavController().navigate(action)
             }
         }
@@ -223,13 +233,13 @@ class LogDetailFragment : BottomSheetDialogFragment() {
     }
 
     private fun setCloseButton() {
-        binding.btnClose.setOnClickListener {
+        binding.ibClose.setOnClickListener {
             findNavController().navigateUp()
         }
     }
 
     private fun setShareButton() {
-        binding.btnShare.setOnClickListener {
+        binding.ibShare.setOnClickListener {
             shareContent(getDynamicLink())
         }
     }
