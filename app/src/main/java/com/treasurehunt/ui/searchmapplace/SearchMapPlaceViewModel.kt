7922a0727ec4d1eadc80143b348533b5d5b0a5b6
jungkeunking <jungkeunking@gmail.com>
2024-03-28 19:03:55 +0900

    remove: 불필요한 클래스, 생성자 파라미터, 임포트 제거

--- app/src/main/java/com/treasurehunt/ui/searchmapplace/SearchMapPlaceViewModel.kt
+++ app/src/main/java/com/treasurehunt/ui/searchmapplace/SearchMapPlaceViewModel.kt
@@ -1,31 +1,16 @@
 package com.treasurehunt.ui.searchmapplace
 
 import androidx.lifecycle.ViewModel
-import com.treasurehunt.data.LogRepository
 import com.treasurehunt.data.MapPlaceSearchRepository
-import com.treasurehunt.data.PlaceRepository
-import com.treasurehunt.data.UserRepository
 import com.treasurehunt.data.remote.model.toMapPlaceModel
 import com.treasurehunt.ui.model.MapPlaceModel
-import com.treasurehunt.ui.model.MapPlaceSearchUiState
-import com.treasurehunt.util.ConnectivityRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchMapPlaceViewModel @Inject constructor(
-    private val logRepo: LogRepository,
-    private val placeRepo: PlaceRepository,
-    private val userRepo: UserRepository,
-    private val mapPlaceSearchRepo: MapPlaceSearchRepository,
-    private val connectivityRepo: ConnectivityRepository
-) :
-    ViewModel() {
-
-    private val _uiState = MutableStateFlow(MapPlaceSearchUiState(listOf()))
-    val uiState: StateFlow<MapPlaceSearchUiState> = _uiState
+    private val mapPlaceSearchRepo: MapPlaceSearchRepository
+) : ViewModel() {
 
     suspend fun search(keyword: String): List<MapPlaceModel> {
         return mapPlaceSearchRepo.getMapPlaceByKeyword(keyword)
